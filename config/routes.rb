09d21849e87057392fded12a8f046a9425db3193
cmac1223 <chrismac539@gmail.com>
2017-07-05 13:05:24 -0400

    show route complete just have to create the view for it

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ root "creatures#index"
 get "/creatures", to: "creatures#index", as: "creatures"
 get "/creatures/new", to: "creatures#new", as: "new_creatures"
 post "/creatures", to: "creatures#create"
+get "/creature/:id", to: "creature#show", as: "creature"
 end
