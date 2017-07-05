dc56fb9a22b398f6a1a97302967fe4b0598ecdf6
cmac1223 <chrismac539@gmail.com>
2017-07-05 14:31:29 -0400

    added edit update route along with an edit view

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -13,12 +13,16 @@ class CreaturesController < ApplicationController
   def create 
     # white list params
     creature_params = params.require(:creature).permit(:name, :description)
+
     # create a new creature in the database from the params
     creature = Creature.new(creature_params)
-    # if creature saves, redirect a route that shows all creatures
+
+    # if creature saves, redirect to route that displays
+    # ONLY the newly created creature
     if creature.save
-      redirect_to creatures_path
-      # redirect_to creature_path is equivalent to redirect_to "/creatures"
+      redirect_to creatures_path(creature)
+      # redirect_to creature_path(creature) is equivalent to:
+      # redirect_to creature "/creatures/#{creature.id} "
     end
   end
 
@@ -33,6 +37,17 @@ class CreaturesController < ApplicationController
     # render the show view (it has access to instance variable)
     # remember the default behavior is to render :show
     
+  end
+
+  # show the edit creature form
+  def edit
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find creature in the database
+    # and save it to the instance variable
+    @creature = Creature.find_by_id(creature_id)
+    # render the edit view (it has access to instance variable)
   end
 end
      
